c063a946a932f30bb0498d99edd6b94657d2dbf4
syinGit <syin1@chicagobooth.edu>
2021-07-18 19:56:58 +0000

    omnicalc-2 (add subtract and multiply done!)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,9 @@ class ApplicationController < ActionController::Base
     render({ :template => "calc_templates/subtract_form.html.erb"})
   end
 
-  # def multiplication_form
-  #   render({ :template => "calc_templates/multiplication_form.html.erb"})
-  # end
+  def multiplication_form
+    render({ :template => "calc_templates/multiply_form.html.erb"})
+  end
 
   # def division_form
   #   render({ :template => "calc_templates/division_form.html.erb"})
@@ -30,6 +30,14 @@ class ApplicationController < ActionController::Base
     @subtract_result = @subtract_first_num - @subtract_second_num
 
     render({ :template => "calc_templates/subtraction.html.erb"})
+  end
+
+  def multiplication
+    @multiply_first_num = params.fetch("multiply_first_num").to_f
+    @multiply_second_num = params.fetch("multiply_second_num").to_f
+    @multiply_result = @multiply_first_num * @multiply_second_num
+
+    render({ :template => "calc_templates/multiplication.html.erb"})
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ get("/add", { :controller => "application", :action => "addition_form"})
 
 get("/subtract", { :controller => "application", :action => "subtraction_form"})
 
-# get("/multiply", { :controller => "application", :action => "multiplication_form"})
+get("/multiply", { :controller => "application", :action => "multiplication_form"})
 
 # get("/divide", { :controller => "application", :action => "division_form"})
 
@@ -15,7 +15,7 @@ get("/wizard_add", { :controller => "application", :action => "addition"})
 
 get("/wizard_subtract", { :controller => "application", :action => "subtraction"})
 
-# get("/wizard_multiply", { :controller => "application", :action => "multiplication"})
+get("/wizard_multiply", { :controller => "application", :action => "multiplication"})
 
 # get("/wizard_divide", { :controller => "application", :action => "division"})
 
