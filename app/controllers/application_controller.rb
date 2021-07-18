3816a94e929013a866a6e576a5080b78f968c709
syinGit <syin1@chicagobooth.edu>
2021-07-18 19:43:59 +0000

    omnicalc-2 (add and subtract done!)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,35 @@
 class ApplicationController < ActionController::Base
 
+  def addition_form
+    render({ :template => "/calc_templates/add_form.html.erb"})
+  end
+
+  def subtraction_form
+    render({ :template => "calc_templates/subtract_form.html.erb"})
+  end
+
+  # def multiplication_form
+  #   render({ :template => "calc_templates/multiplication_form.html.erb"})
+  # end
+
+  # def division_form
+  #   render({ :template => "calc_templates/division_form.html.erb"})
+  # end  
+
+  def addition
+    @add_first_num = params.fetch("add_first_num").to_f
+    @add_second_num = params.fetch("add_second_num").to_f
+    @add_result = @add_first_num + @add_second_num
+
+    render({ :template => "calc_templates/addition.html.erb"})
+  end
+
+  def subtraction
+    @subtract_first_num = params.fetch("subtract_first_num").to_f
+    @subtract_second_num = params.fetch("subtract_second_num").to_f
+    @subtract_result = @subtract_first_num - @subtract_second_num
+
+    render({ :template => "calc_templates/subtraction.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+get("/add", { :controller => "application", :action => "addition_form"})
+
+get("/subtract", { :controller => "application", :action => "subtraction_form"})
+
+# get("/multiply", { :controller => "application", :action => "multiplication_form"})
+
+# get("/divide", { :controller => "application", :action => "division_form"})
+
+
+get("/wizard_add", { :controller => "application", :action => "addition"})
+
+get("/wizard_subtract", { :controller => "application", :action => "subtraction"})
+
+# get("/wizard_multiply", { :controller => "application", :action => "multiplication"})
+
+# get("/wizard_divide", { :controller => "application", :action => "division"})
+
 end
